4a432a15495c977ad90fe1e5858eaad8de0e3bd7
RaghavSakhuja <raghavsakhuja03@gmail.com>
2022-10-25 22:17:29 +0530

    completed date

--- date.c
+++ date.c
@@ -10,7 +10,7 @@
 int main(int argc, char const *argv[])
 {
     int flag1=(!strcmp(argv[1],"x")); //-I
-	int flag2=(!strcmp(argv[2],"x")); //-u
+	int flag2=(!strcmp(argv[2],"x")); //-u`
     
     time_t t=time(NULL);
     struct tm curtime=*localtime(&t);
@@ -21,7 +21,7 @@ int main(int argc, char const *argv[])
     else if(flag2){
         struct tm utc=*gmtime(&t);
         char* days[7]={"Sun","Mon","Tue","Wed","Thu","Fri","Sat"};
-        char* months[12]={"Jan","Feb","Mar","Apr","May","Jun","Jul","Aug",."Sep","Oct","Nov","Dec"};
+        char* months[12]={"Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"};
         printf("%s %s %d %d:%d:%d UTC %d\n",days[utc.tm_wday],months[utc.tm_mon],utc.tm_mday,utc.tm_hour,utc.tm_min,utc.tm_sec,utc.tm_year+1900);
     }
     else{
